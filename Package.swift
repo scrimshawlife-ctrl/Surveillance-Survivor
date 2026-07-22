09aa2fee3dd1e7a324136d512d57df6584b6b0e2
Daniel Meyer <scrimshawlife@gmail.com>
2026-07-21 23:08:37 -0700

    add deterministic Swift core package

---
+++ Package.swift
@@ -0,0 +1,12 @@
+// swift-tools-version: 6.0
+import PackageDescription
+
+let package = Package(
+    name: "SurveillanceCore",
+    platforms: [.iOS(.v18), .macOS(.v15)],
+    products: [.library(name: "SurveillanceCore", targets: ["SurveillanceCore"])],
+    targets: [
+        .target(name: "SurveillanceCore"),
+        .testTarget(name: "SurveillanceCoreTests", dependencies: ["SurveillanceCore"])
+    ]
+)
